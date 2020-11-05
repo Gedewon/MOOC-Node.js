1e50d823094db7f43d5d3443dc2e9df9ee627a0c
Gedewon <gedewon.hayle.9@gmail.com>
2020-11-05 11:14:04 +0300

    express and http server

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// var rect = {
-// 	perimeter: (x, y) => (2*(x+y)),
-// 	area: (x, y) => (x*y)
-// };
+
 var rect = require('./rectangle');
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
@@ -17,6 +14,3 @@ function solveRect(l,b) {
 }
 
 solveRect(2,4);
-// solveRect(3,5);
-// solveRect(0,5);
-// solveRect(-3,5);
